8a55ef62036efac73e2387e901332ed988edb02a
Mauro-Prencipe <49814184+Mauro-Prencipe@users.noreply.github.com>
2021-03-29 18:32:06 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'BM3-thermal'
+project = 'QM-Thermodynamics'
 copyright = '2021, Mauro Prencipe'
 author = 'Mauro Prencipe'
 
